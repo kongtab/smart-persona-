fceb0545ecb43fd3a40d1b83c513dfa5863bd560
tae <siwawong.toe@spumail.net>
2025-11-19 13:04:12 +0700

    แก้

--- smart persona/src/pages/LoginPage.jsx
+++ smart persona/src/pages/LoginPage.jsx
@@ -13,9 +13,9 @@ export default function LoginPage() {
         <div className="container">
           <div className="header-title">PerFile</div>
         </div>
-         <button className="back-btn">
-        <span className="icon">⟵</span> กลับเข้าสู่หน้าหลัก
-      </button>
+        <button className="back-btn">
+          <span className="icon">⟵</span> กลับเข้าสู่หน้าหลัก
+        </button>
       </header>
 
       <main className="page-wrapper">
@@ -28,47 +28,39 @@ export default function LoginPage() {
             <p className="mb-8 muted-small">ต้องการเข้าสู่ระบบด้วยอะไร?</p>
 
             <div className="provider-wrapper2">
-              <button className="provider-btn provider-google" type="button">
-                <span className="provider-left" aria-hidden>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 533.5 544.3"
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden
-                    focusable="false"
-                  >
-                    <path
-                      fill="#4285F4"
-                      d="M533.5 278.4c0-18.5-1.5-36.2-4.3-53.5H272v101.4h147.1c-6.3 34.1-25 62.9-53.4 82v68.2h86.1c50.4-46.4 81.7-115 81.7-198.1z"
-                    />
-                    <path
-                      fill="#34A853"
-                      d="M272 544.3c72.6 0 133.6-24.1 178.1-65.6l-86.1-68.2c-23.9 16-54.6 25.3-92 25.3-70.6 0-130.3-47.6-151.6-111.6H34.9v69.9C79.3 488.1 168.3 544.3 272 544.3z"
-                    />
-                    <path
-                      fill="#FBBC05"
-                      d="M120.4 326.2c-10.9-32.8-10.9-68.3 0-101.1V155.2H34.9C12.4 198.4 0 240.4 0 272.5s12.4 74.1 34.9 117.3l85.5-63.6z"
-                    />
-                    <path
-                      fill="#EA4335"
-                      d="M272 107.2c39.5 0 75 13.6 103 40.3l77.4-77.4C403.9 24 344.9 0 272 0 168.3 0 79.3 56.2 34.9 155.2l85.5 69.9C141.7 154.8 201.4 107.2 272 107.2z"
-                    />
-                  </svg>
-                </span>
-                <span className="provider-label2">
-                  ดำเนินการต่อด้วยบัญชี Google
-                </span>
+              <button className="google-btn">
+                {/* ⬇️ โค้ด SVG ไอคอน Google ใหม่ ⬇️ */}
+                <svg viewBox="0 0 48 48" width="20" height="20" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg">
+                  <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.15 30.49 0 24 0 14.65 0 6.68 5.3 3.29 13.01l7.66 5.86C12.59 13.78 17.81 9.5 24 9.5z" />
+                  <path fill="#4285F4" d="M46.75 24.53c0-1.54-.14-3.04-.41-4.5H24v8.52h12.55c-.56 3.84-2.88 7.03-6.14 9.17l7.66 5.86C43.51 39.15 46.75 32.32 46.75 24.53z" />
+                  <path fill="#FBBC05" d="M10.95 29.17c-.52-1.54-.81-3.19-.81-4.87s.29-3.33.81-4.87L3.29 13.01C1.22 17.02 0 20.65 0 24.53s1.22 7.51 3.29 11.52l7.66-5.87z" />
+                  <path fill="#34A853" d="M24 48c6.48 0 11.96-2.13 15.96-5.73l-7.66-5.86c-2.48 1.6-5.6 2.54-8.3 2.54-6.19 0-11.41-4.28-13.3-10.04l-7.66 5.86C6.68 42.7 14.65 48 24 48z" />
+                  <path fill="none" d="M0 0h48v48H0z" />
+                </svg>
+                {/* ⬆️ โค้ด SVG ไอคอน Google ใหม่ ⬆️ */}
+                ดำเนินการด้วยบัญชี Google
               </button>
 
-              <button className="provider-btn provider-linkedin" type="button">
-                <span className="provider-left" aria-hidden>
-                  <span className="icon-square linkedin">in</span>
-                </span>
-                <span className="provider-label2">
-                  ดำเนินการต่อด้วยบัญชี LinkedIn
-                </span>
+                  <button className="google-btn">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                      focusable="false"
+                      role="img"
+                    >
+                      <path fill="#0A66C2" d="M22.23 0H1.77C.79 0 0 .77 0 1.72v20.56C0 23.23.79 24 1.77 24h20.46c.98 
+          0 1.77-.77 1.77-1.72V1.72C24 .77 23.21 0 22.23 0z"/>
+                      <path fill="#fff" d="M7.09 20.45H3.56V9h3.53v11.45zM5.33 7.59c-1.13 0-2.05-.91-2.05-2.04a2.05 
+          2.05 0 1 1 4.1 0c0 1.13-.92 2.04-2.05 2.04zm15.12 12.86h-3.53v-5.6c0-1.33-.03-3.04-1.85-3.04-1.85 
+          0-2.13 1.45-2.13 2.95v5.69h-3.53V9h3.39v1.56h.05c.47-.9 1.62-1.85 3.34-1.85 3.57 0 4.23 2.35 4.23 
+          5.41v6.33z"/>
+                    </svg>
+                    ดำเนินการต่อด้วยบัญชี LinkedIn
               </button>
+
             </div>
 
             <div class="divider-or">
